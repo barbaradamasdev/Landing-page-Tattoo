992ee943eaf0544bc83c1d450e6052415bb892f4
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-05-10 09:19:47 -0300

    complete css

--- script.js
+++ script.js
@@ -12,20 +12,41 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.getElementById("nav").style.padding = "15px 20px";
+      document.getElementById("nav").style.padding = "0px 30px";
       document.getElementById("nav").style.transition = "1s";
-      document.getElementById("logo").style.fontSize = "20px";
+      document.getElementById("logo").style.fontSize = "25px";
       document.getElementById("logo").style.transition = "1s";
       document.getElementById("nav-secondary").style.top = "52px";
       document.getElementById("close-menu").style.top = "57px";
     } else {
-        document.getElementById("nav").style.padding = "30px";
-        document.getElementById("logo").style.fontSize = "20px";
+        document.getElementById("nav").style.padding = "15px 30px";
+        document.getElementById("logo").style.fontSize = "clamp(25px, 6vw, 35px)";
         document.getElementById("nav-secondary").style.top = "83px";
         document.getElementById("close-menu").style.top = "83px";
     }
   }
 
-  function zoom (element) {
+  /* function zoom (element) {
     element.classList.toggle('active');
+  } */
+
+  function showMore() {
+    document.getElementById('img01').classList.toggle('active');
+    document.getElementById('img02').classList.toggle('active');
+    document.getElementById('img03').classList.toggle('active');
+    document.getElementById('img04').classList.toggle('active');
+    document.getElementById('img05').classList.toggle('active');
+    document.getElementById('img06').classList.toggle('active');
+    document.getElementById('img07').classList.toggle('active');
+    document.getElementById('img08').classList.toggle('active');
+    document.getElementById('img09').classList.toggle('active');
+    document.getElementById('img10').classList.toggle('active');
+    document.getElementById('img11').classList.toggle('active');
+    document.getElementById('img12').classList.toggle('active');
+    document.getElementById('img13').classList.toggle('active');
+    document.getElementById('showMore').style.display ='none';
+  }
+
+  function formsent() {
+    document.getElementById('formenviado').classList.toggle('active');
   }
